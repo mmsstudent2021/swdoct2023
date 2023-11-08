17b602fea87cf5ba95d46f6fa09f87fbf139c68c
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-11-08 16:34:09 +0630

    tailwind update

---
+++ 3-tailwincss/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "nav.html",
+    "introl.html",
+    "buttons.html",
+    "table.html",
+    "theme.html",
+    "index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
