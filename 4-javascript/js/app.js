cadb4c1b7dffd3701fe9a7be49fda8126cc799ef
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-12-08 15:49:43 +0630

    up

---
+++ 4-javascript/js/app.js
@@ -0,0 +1,139 @@
+// function run() {
+//   console.log("*");
+//   console.log("* *");
+//   console.log("* * *");
+//   return "triangle";
+//   console.log("hello");
+// }
+
+// console.log(run());
+
+// find my bd year, u must need to know age
+// logic => 2023 - age
+
+// my age - 29
+// mgmg age - 15
+// su su age - 18
+// hla hla age - 25
+
+// function findBirthYear(currentAge=0,name="My test"){
+//     // console.log(currentAge);
+//     const currentYear = 2023;
+//     const birthYear = currentYear - currentAge;
+//     return `${name} was born in ${birthYear}`;
+// }
+
+// console.log(findBirthYear(29,"hein htet zan"));
+// console.log(findBirthYear(15, "mg mg"));
+// console.log(findBirthYear(18, "su su"));
+// console.log(findBirthYear(25, "hla hla"));
+
+// console.log(findBirthYear());
+
+//
+
+// function area(width=0, breadth=0) {
+//     const result = width * breadth;
+//   return `width ${width}ft \nbreadth ${breadth}ft\n-------\narea ${result} sqft`;
+// }
+
+// console.log(area(17,60));
+// console.log(area(17,50));
+// console.log(area(25,50));
+// console.log(area());
+
+// const arr = [];
+// const obj = {};
+
+// console.log(typeof arr);
+// console.log(typeof obj);
+
+// const fruits = ["apple","orange","mango","banana","lemon"];
+// //console.log(fruits[2]);// access element
+// fruits[2] = "lime" // overwrite element
+// console.log(fruits);
+// console.log(typeof fruits);
+
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+// console.log(fruits[0]);
+
+// const arr = [];
+
+// arr[0] = "aaa";
+// arr[1] = "bbb";
+// arr[2] = "ccc";
+// arr[3] = "ddd";
+// arr[4] = "eee";
+// // arr[2] = "zzz";
+// // arr[10] = "xxx"
+
+// console.log(arr);
+
+// obj (key : value) set
+// myself
+// name : "hein htet zan"
+// age : 29
+// township : "hlaing"
+
+// car
+// model : toyota bz3
+// eng : 1.5
+// color : ["gray","white","black"]
+
+// const myself = {
+//     name : "hein htet zan",
+//     age : 29,
+//     township : "hlaing",
+//     job : "developer",
+//     skill : ["html","css","js"]
+// }
+
+// console.log(myself);
+// console.log(typeof myself);
+
+// // access property
+// console.log(myself["name"]);
+// console.log(myself["age"]);
+// console.log(myself["township"]);
+
+// // dot
+// console.log(myself.name);
+// console.log(myself.age);
+// console.log(myself.job);
+
+// // overwrite
+// myself.name = "Ko Htet";
+// myself.age = 24;
+// console.log(myself);
+
+// const obj = {};
+
+// obj["a"] = "aaa";
+// obj.b = "bbb";
+// obj.c = "ccc";
+// obj.d = "ddd";
+// obj.c = "zzz";
+
+// console.log(obj);
+
+// const x = 5;
+// const y = 10;
+
+// console.log(x > y);
+// console.log(x < y);
+
+// console.log(x == y);
+// console.log(x == 5);
+// console.log(x == "5");
+
+// //strict mode
+
+// console.log(x === 5);
+// console.log(x === "5");
+
+// const x = null;
+// console.log(x);
+
+// let x;
+// console.log(x);
