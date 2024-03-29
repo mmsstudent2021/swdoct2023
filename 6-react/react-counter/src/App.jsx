9128dd82dd26f9de949ceee5fce3e7b786a5c7a1
mmsstudent2021 <mmsstudent2021@gmail.com>
2024-03-29 17:09:32 +0700

    faq finish

--- 6-react/react-counter/src/App.jsx
+++ 6-react/react-counter/src/App.jsx
@@ -3,7 +3,45 @@ import Counter from "./Counter";
 import Box from "./Box";
 
 const App = () => {
-  return <Box />;
+  const faqs = [
+    {
+      id: 1,
+      question: "What is Lorem Ipsum?",
+      answer:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      id: 2,
+      question: "Why do we use it?",
+      answer:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    },
+    {
+      id: 3,
+      question: "Where does it come from?",
+      answer:
+        "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+    },
+    {
+      id: 4,
+      question: "What is the purpose of Lorem Ipsum?",
+      answer:
+        "It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+    },
+    {
+      id: 5,
+      question: "Is Lorem Ipsum always used?",
+      answer: "No, Lorem Ipsum comes in many forms and variations.",
+    },
+  ];
+
+  return (
+    <div>
+      {faqs.map((faq) => (
+        <Box key={faq.id} question={faq.question} answer={faq.answer} />
+      ))}
+    </div>
+  );
 };
 
 export default App;
--- 6-react/react-counter/src/Box.jsx
+++ 6-react/react-counter/src/Box.jsx
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Box = () => {
+const Box = (props) => {
+  const [hide, setHide] = useState(true);
+  const handleOpen = () => {
+    setHide(false);
+  };
+
+  const handleClose = () => {
+    setHide(true);
+  };
+
+  const handleToggle = () => {
+    setHide(!hide);
+  };
   return (
-    <div className="grid grid-cols-2 p-5 gap-5">
-      <button className=" py-3 rounded-lg bg-gray-400  active:scale-90 duration-200 text-white col-span-1 border text-center">
-        Open
+    <div className="grid grid-cols-2 px-5 py-3 gap-3">
+      <button
+        onClick={handleToggle}
+        className=" py-3 rounded-lg bg-blue-400  active:scale-90 duration-200 text-white col-span-2 border flex items-center justify-between gap-3 px-5"
+      >
+        <h1 className="font-serif font-bold text-2xl">{props.question}</h1>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={`w-6 h-6 duration-200 ${!hide && "rotate-180"}`}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
+          />
+        </svg>
       </button>
-      <button className=" py-3 rounded-lg bg-gray-400 active:scale-90 duration-200 text-white col-span-1 border text-center">
-        Close
-      </button>
-      <div className="p-5 rounded-lg bg-gray-100 col-span-2 border   ">
-        <h1 className="font-serif font-bold text-3xl">Content Box</h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium,
-          placeat quo id, eveniet voluptatem magni culpa assumenda itaque
-          aliquam accusamus doloremque libero consequatur nisi! Vero dolores
-          ipsa magni ut doloremque!
-        </p>
+
+      <div
+        className={` ${
+          hide && "hidden"
+        } p-5 rounded-lg bg-gray-100 col-span-2 border`}
+      >
+        <p>{props.answer}</p>
       </div>
     </div>
   );
